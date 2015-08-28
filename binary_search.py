aa7c8099dd559e2c3860900de4837194d572cea7
Melvin <melvin0008@gmail.com>
2015-08-27 20:20:51 -0400

    add recursive binary search

---
+++ binary_search.py
@@ -0,0 +1,18 @@
+from mergesort import mergesort
+
+def binary_search(a,s):
+	a=mergesort(a)
+	n=len(a)
+	if n>0:
+		mid=a[n/2]
+		if mid ==s:
+			return True
+		elif s < mid:
+			return binary_search(a[:n/2],s)
+		elif s> mid:
+			return binary_search(a[(n/2)+1:],s)
+	return False
+
+print binary_search([5,20,1,8,25,80],7)
+
+
